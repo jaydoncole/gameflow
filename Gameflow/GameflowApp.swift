b7d81f444f74c91ca50424fa4ffeae75852aa4f3
Jaydon Cole <jaydon@sitenation.com>
2025-09-27 20:45:55 -0500

    Initial Commit

---
+++ Gameflow/GameflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameflowApp.swift
+//  Gameflow
+//
+//  Created by Jaydon Cole on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GameflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
